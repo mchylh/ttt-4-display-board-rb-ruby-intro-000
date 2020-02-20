b202fa113522c52239952a29b1ce8f7ec3b1cdb8
Michaylah Mam <mchylh@gmail.com>
2020-02-20 18:56:57 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,107 +3,107 @@
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------------------------------------"
+  puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------------------------------------"
+  puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 board = [" "," "," "," ","X"," "," "," "," "]
 def display_board(board)
-  puts "   |   |   "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   | X |   "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 board = ["O"," "," "," "," "," "," "," "," "]
 def display_board(board)
-  puts " O |   |   "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
 def display_board(board)
-  puts " O |   |   "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   | X |   "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
 def display_board(board)
-  puts " X | X | X "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
 def display_board(board)
-  puts "   |   |   "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " O | O | O "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
 def display_board(board)
-  puts " X |   |   "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   | X |   "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "   |   | X "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
 def display_board(board)
-  puts "   |   | O "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   | O |   "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " O |   |   "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
 def display_board(board)
-  puts " X | X | X "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts " X | O | O "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " X | O | O "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
 def display_board(board)
-  puts " X | O | X "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts " O | X | X "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " O | X | O "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
 def display_board(board)
-  puts " X | X | X "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts " X | X | X "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " X | X | X "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
 def display_board(board)
-  puts " O | O | O "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts " O | O | O "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " O | O | O "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
